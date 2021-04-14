d1858534a598a8f17cc576deb91ee42d9da3497e
Kristoffer Jensen <krijen16@student.aau.dk>
2021-04-14 22:48:55 +0200

    ExpenseItem css

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,7 +1,14 @@
-function ExpenseItem(){
-	return(
-		<h2>Expense Item!</h2>
-	);
+import './ExpenseItem.css'
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div>
+        <h2 className="expense-item__descriptor">Car Insurance</h2>
+        <div className="expense-item__price">$296.47</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
