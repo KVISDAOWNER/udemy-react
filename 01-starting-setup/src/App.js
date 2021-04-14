bd8fe81b5113266d390adebed504e958ecf18771
Kristoffer Jensen <krijen16@student.aau.dk>
2021-04-14 23:07:58 +0200

    passing data to component with props

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,10 +1,45 @@
-import ExpenseItem from './components/ExpenseItem';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (//jsx code "html inside js"
-    <div>		
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 293,
+      date: new Date(2021, 4, 14),
+    },
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 293,
+      date: new Date(2021, 4, 14),
+    },
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 293,
+      date: new Date(2021, 4, 14),
+    },
+  ];
+  return (
+    //jsx code "html inside js"
+    <div>
       <h2>Let's get started!</h2>
-			<ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+			<ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+			<ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
     </div>
   );
 }
