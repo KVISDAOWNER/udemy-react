289fbcaa357b10106f568b499c0f34df70e30cb3
Kristoffer Jensen <krijen16@student.aau.dk>
2021-04-14 22:57:24 +0200

    First dynamic data! :D

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,11 +1,15 @@
-import './ExpenseItem.css'
+import "./ExpenseItem.css";
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+	const expenseTitle = 'Car Insurance';
+	const expenseAmount = 297.64;
+	
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
-      <div>
-        <h2 className="expense-item__descriptor">Car Insurance</h2>
-        <div className="expense-item__price">$296.47</div>
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__descriptor">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
