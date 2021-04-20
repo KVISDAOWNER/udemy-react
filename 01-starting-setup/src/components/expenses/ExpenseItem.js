5c8bd4fdba4cf02801a036c8142f59d3dfa9c75c
KVISDAWONER <krijen16@student.aau.dk>
2021-04-20 21:47:15 +0200

    simple button that change html content dynamically

--- 01-starting-setup/src/components/expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/expenses/ExpenseItem.js
@@ -1,15 +1,22 @@
+import React, {useState} from 'react';
+
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
+	let [title, setTitle] = useState(props.title); //react hook - for updating html content on change - returns the variable itself and the update func
 
+	function clickHandler(){
+		setTitle('clicked!!');
+	}
 	return (
 		<Card className='expense-item'>
 			<ExpenseDate date={props.date} />
 			<div className='expense-item__descriptor'>
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className='expense-item__price'>${props.amount}</div>
+				<button onClick={clickHandler}>Change Title</button>
 			</div>
 		</Card>
 	);
