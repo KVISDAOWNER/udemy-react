d977ec5fc70a5ed516b1f2e53dc60fc9e1c4b3f2
KVISDAWONER <krijen16@student.aau.dk>
2021-04-20 20:41:33 +0200

    spaces to tabs

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -2,33 +2,33 @@ import Expenses from "./components/Expenses";
 
 function App() {
 	const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 293,
-      date: new Date(2021, 4, 14),
-    },
-    {
-      id: "e2",
-      title: "Dishwasher",
-      amount: 1000,
-      date: new Date(2021, 4, 14),
-    },
-    {
-      id: "Netflix",
-      title: "Car Insurance",
-      amount: 10,
-      date: new Date(2021, 4, 14),
-    },
-  ];
+		{
+			id: "e1",
+			title: "Car Insurance",
+			amount: 293,
+			date: new Date(2021, 4, 14),
+		},
+		{
+			id: "e2",
+			title: "Dishwasher",
+			amount: 1000,
+			date: new Date(2021, 4, 14),
+		},
+		{
+			id: "Netflix",
+			title: "Car Insurance",
+			amount: 10,
+			date: new Date(2021, 4, 14),
+		},
+	];
 
-  return (
-    //jsx code "html inside js"
-    <div>
-      <h2>Let's get started!</h2>
-      <Expenses expenses={expenses}></Expenses>
-    </div>
-  );
+	return (
+		//jsx code "html inside js"
+		<div>
+			<h2>Let's get started!</h2>
+			<Expenses expenses={expenses}></Expenses>
+		</div>
+	);
 }
 
 export default App;
--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -3,15 +3,15 @@ import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props) {
 
-  return (
-    <div className="expense-item">
+	return (
+		<div className="expense-item">
 			<ExpenseDate date={props.date} />
-      <div className="expense-item__descriptor">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-    </div>
-  );
+			<div className="expense-item__descriptor">
+				<h2>{props.title}</h2>
+				<div className="expense-item__price">${props.amount}</div>
+			</div>
+		</div>
+	);
 }
 
 export default ExpenseItem;
