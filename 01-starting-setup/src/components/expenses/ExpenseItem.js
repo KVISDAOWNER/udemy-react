b9bd3ddc32a8cedc25441aeef69ec3a925ddaf30
KVISDAWONER <krijen16@student.aau.dk>
2021-04-20 21:53:53 +0200

    className for expense-item_description misspelled

--- 01-starting-setup/src/components/expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/expenses/ExpenseItem.js
@@ -13,7 +13,7 @@ function ExpenseItem(props) {
 	return (
 		<Card className='expense-item'>
 			<ExpenseDate date={props.date} />
-			<div className='expense-item__descriptor'>
+			<div className='expense-item__description'>
 				<h2>{title}</h2>
 				<div className='expense-item__price'>${props.amount}</div>
 				<button onClick={clickHandler}>Change Title</button>
