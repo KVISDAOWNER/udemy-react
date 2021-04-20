0ab6d70a62ff0284176ae4666cad0580a141875e
KVISDAWONER <krijen16@student.aau.dk>
2021-04-20 21:00:34 +0200

    extracted css styling into wrapper component Card

---
+++ 01-starting-setup/src/components/Card.js
@@ -0,0 +1,9 @@
+import "./Card.css"
+function Card(props){
+	const classes = 'card ' + props.className; //othwerwise we lose the classnames given to it outside of this file
+	return(
+		<div className={classes}>{props.children}</div> //.children is reserved - it is the content between open and close tag
+	)
+}
+
+export default Card;
