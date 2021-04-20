4be85e7729361630e6bece170d96d2b66b64d2ad
KVISDAWONER <krijen16@student.aau.dk>
2021-04-20 22:28:13 +0200

    ExpenseForm updated state for each input change

--- 01-starting-setup/src/components/newExpenses/ExpenseForm.js
+++ 01-starting-setup/src/components/newExpenses/ExpenseForm.js
@@ -1,9 +1,24 @@
+import {useState} from 'react';
 import './ExpenseForm.css'
 
 function ExpenseForm(){
-
+	
+	const [title, setTitle] = useState(''); //initially title is empty aka. ''
+	
 	function titleChangedHandler(event){
-		console.log(event.target.value);
+		setTitle(event.target.value);
+	}
+
+	const [amount, setAmount] = useState(''); //initially amount is '' not a number because it always returns a string anyway
+	
+	function amountChangedHandler(event){
+		setAmount(event.target.value);
+	}
+
+	const [date, setDate] = useState(''); //initially date is empty aka. ''
+	
+	function dateChangedHandler(event){
+		setDate(event.target.value);
 	}
 
 	return (
@@ -15,11 +30,11 @@ function ExpenseForm(){
 				</div>
 				<div className='new-expense__control'>
 					<label>Amount</label>
-					<input type='number' min='0.01' step='0.01'/>
+					<input type='number' min='0.01' step='0.01' onChange={amountChangedHandler}/>
 				</div>
 				<div className='new-expense__control'>
 					<label>Date</label>
-					<input type='date' min='2019-01-01' step='2022-12-31'/>
+					<input type='date' min='2019-01-01' step='2022-12-31' onChange={dateChangedHandler}/>
 				</div>
 			</div>
 			<div className='new-expense__actions'>
