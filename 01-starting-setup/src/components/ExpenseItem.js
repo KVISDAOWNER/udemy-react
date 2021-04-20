205abf3ab07c7f157f86e06879f6902f96064b07
KVISDAWONER <krijen16@student.aau.dk>
2021-04-20 20:27:31 +0200

    Splitted date into ExpenseDate component + added css

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,11 @@
 import "./ExpenseItem.css";
+import ExpenseDate from './ExpenseDate'
+
 function ExpenseItem(props) {
-  const expenseDate = new Date(2021, 2, 28);
-	const expenseTitle = 'Car Insurance';
-	const expenseAmount = 297.64;
-	
+
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+			<ExpenseDate date={props.date} />
       <div className="expense-item__descriptor">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
