41f77ea4b7fc1540c5b68aa58bc3369e2e07171f
KVISDAWONER <krijen16@student.aau.dk>
2021-04-21 16:04:45 +0200

    remove demo button and hook/useState from ExpenseItem

--- 01-starting-setup/src/components/expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/expenses/ExpenseItem.js
@@ -5,18 +5,12 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 function ExpenseItem(props) {
-	let [title, setTitle] = useState(props.title); //react hook - for updating html content on change - returns the variable itself and the update func
-
-	function clickHandler(){
-		setTitle('clicked!!');
-	}
 	return (
 		<Card className='expense-item'>
 			<ExpenseDate date={props.date} />
 			<div className='expense-item__description'>
 				<h2>{title}</h2>
 				<div className='expense-item__price'>${props.amount}</div>
-				<button onClick={clickHandler}>Change Title</button>
 			</div>
 		</Card>
 	);
