cc51dbd28bc729be39b44ddccaa6060dca96de11
KVISDAWONER <krijen16@student.aau.dk>
2021-04-26 22:01:40 +0200

    NewUserForm state and handlers

--- create-user/src/components/NewUserForm.js
+++ create-user/src/components/NewUserForm.js
@@ -1,16 +1,44 @@
+
+import { useState } from 'react';
 import style from './NewUserForm.module.css'
+
+
 function NewUserForm(props){
+	const [username, setUsername] = useState('');
+	const [age, setAge] = useState('');
+	
+
+	function usernameChangedHandler(event){
+		setUsername(event.target.value);
+	}
+
+	function ageChangedHandler(event){
+		setAge(event.target.value);
+	}
+
+	function submitHandler(event){
+		event.preventDefault(); //dont reload page on form submit - this is otherwise default html behavior
+
+		const userData = {
+			id: username,
+			username: username,
+			age: age
+		};
+
+		console.log(userData);
+	}
+
 	return (
 		<div className={style['new-user']}>
-			<form>
+			<form onSubmit={submitHandler}>
 				<div className={style['new-user__controls']}>
 					<div className={style['new-user__control']}>
 						<label>Username</label>
-						<input type='text'></input>
+						<input type='text' onChange={usernameChangedHandler}></input>
 					</div>
 					<div className={style['new-user__control']}>
-						<label>Age (Years)</label>
-						<input type='text'></input>
+						<label >Age (Years)</label>
+						<input type='text' onChange={ageChangedHandler}></input>
 					</div>
 				</div>
 				<div>
