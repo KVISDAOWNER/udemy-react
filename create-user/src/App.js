c1c572586a6890c5ad2c04e5062019510d97dd77
KVISDAWONER <krijen16@student.aau.dk>
2021-04-26 21:01:20 +0200

    init card component

--- create-user/src/App.js
+++ create-user/src/App.js
@@ -1,7 +1,10 @@
+import Card from './components/Card';
 
 function App() {
   return (
     <div>
+			<Card>
+			</Card>
     </div>
   );
 }
---
+++ create-user/src/components/Card.js
@@ -0,0 +1,11 @@
+import style from './Card.module.css'
+
+function Card(){
+	return (
+		<div className={style.card}>
+
+		</div>
+	);
+}
+
+export default Card;
