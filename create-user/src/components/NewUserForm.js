dde3ad0791f84a2bb027e527a2453b8cbc5d83ce
KVISDAWONER <krijen16@student.aau.dk>
2021-04-26 22:15:21 +0200

    name and age input red on empty submit

--- create-user/src/components/NewUserForm.js
+++ create-user/src/components/NewUserForm.js
@@ -1,52 +1,71 @@
+import { useState } from "react";
+import style from "./NewUserForm.module.css";
 
-import { useState } from 'react';
-import style from './NewUserForm.module.css'
+function NewUserForm(props) {
+  const [username, setUsername] = useState("");
+  const [age, setAge] = useState("");
+  const [isUsernameValid, setIsUsernameValid] = useState(true);
+  const [isAgeValid, setIsAgeValid] = useState(true);
 
+  function usernameChangedHandler(event) {
+    setUsername(event.target.value);
+    setIsUsernameValid(true);
+  }
 
-function NewUserForm(props){
-	const [username, setUsername] = useState('');
-	const [age, setAge] = useState('');
-	
+  function ageChangedHandler(event) {
+    setAge(event.target.value);
+    setIsAgeValid(true);
+  }
 
-	function usernameChangedHandler(event){
-		setUsername(event.target.value);
-	}
+  function submitHandler(event) {
+    event.preventDefault(); //dont reload page on form submit - this is otherwise default html behavior
 
-	function ageChangedHandler(event){
-		setAge(event.target.value);
-	}
+    if (username === "") setIsUsernameValid(false);
+    if (age === "") setIsAgeValid(false);
 
-	function submitHandler(event){
-		event.preventDefault(); //dont reload page on form submit - this is otherwise default html behavior
+    const userData = {
+      id: username,
+      username: username,
+      age: age,
+    };
 
-		const userData = {
-			id: username,
-			username: username,
-			age: age
-		};
+    setUsername("");
+    setAge("");
 
-		console.log(userData);
-	}
+    console.log(userData);
+  }
 
-	return (
-		<div className={style['new-user']}>
-			<form onSubmit={submitHandler}>
-				<div className={style['new-user__controls']}>
-					<div className={style['new-user__control']}>
-						<label>Username</label>
-						<input type='text' onChange={usernameChangedHandler}></input>
-					</div>
-					<div className={style['new-user__control']}>
-						<label >Age (Years)</label>
-						<input type='text' onChange={ageChangedHandler}></input>
-					</div>
-				</div>
-				<div>
-					<button type='submit'>Add User</button>
-				</div>
-			</form>
-		</div>
-	)
+  return (
+    <div className={style["new-user"]}>
+      <form onSubmit={submitHandler}>
+        <div className={style["new-user__controls"]}>
+          <div
+            className={`${style["new-user__control"]} ${
+              !isUsernameValid && style.invalid
+            }`}
+          >
+            <label>Username</label>
+            <input
+              type="text"
+              onChange={usernameChangedHandler}
+              value={username}
+            ></input>
+          </div>
+          <div
+            className={`${style["new-user__control"]} ${
+              !isAgeValid && style.invalid
+            }`}
+          >
+            <label>Age (Years)</label>
+            <input type="text" onChange={ageChangedHandler} value={age}></input>
+          </div>
+        </div>
+        <div>
+          <button type="submit">Add User</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default NewUserForm;
